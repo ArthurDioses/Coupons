e95c54e318ae8e3550fa356ffa011a9cf535c8f1
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-12-19 10:00:44 -0500

    Impl() - Implement Room with class.

---
+++ app/src/main/java/com/dioses/coupons/CouponApplication.kt
@@ -0,0 +1,21 @@
+package com.dioses.coupons
+
+import android.app.Application
+import androidx.room.Room
+
+class CouponApplication : Application() {
+
+    companion object {
+        lateinit var database: CouponDatabase
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        database = Room.databaseBuilder(
+            this,
+            CouponDatabase::class.java,
+            "CouponDatabase"
+        ).build()
+    }
+}
