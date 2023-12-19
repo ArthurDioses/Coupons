6db440e555308ceef92a84a096b88b28063ff5f7
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-12-19 09:31:59 -0500

    Fix() - Add ksp for room compiler dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp") version "1.9.10-1.0.13"
 }
 
 android {
@@ -64,7 +65,7 @@ dependencies {
 
     //Room
     implementation("androidx.room:room-runtime:$room_version")
-    annotationProcessor("androidx.room:room-compiler:$room_version")
+    ksp("androidx.room:room-compiler:$room_version")
     // optional - Kotlin Extensions and Coroutines support for Room
     implementation("androidx.room:room-ktx:$room_version")
 }
