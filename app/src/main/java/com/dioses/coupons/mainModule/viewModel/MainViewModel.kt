5893f15d8eb3a6508daae608df021012a8abea9a
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-12-19 23:06:37 -0500

    Impl() - UI layer. - State Holder

--- app/src/main/java/com/dioses/coupons/mainModule/viewModel/MainViewModel.kt
+++ app/src/main/java/com/dioses/coupons/mainModule/viewModel/MainViewModel.kt
@@ -1,4 +1,39 @@
 package com.dioses.coupons.mainModule.viewModel
 
-class MainViewModel {
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dioses.coupons.R
+import com.dioses.coupons.common.entities.CouponEntity
+import com.dioses.coupons.common.utils.getMessageErrorByCode
+import com.dioses.coupons.mainModule.model.MainRepository
+import kotlinx.coroutines.launch
+
+class MainViewModel : ViewModel() {
+    private val repository = MainRepository()
+
+    private val result = MutableLiveData<CouponEntity>()
+    fun getResult() = result
+
+    private val snackbarMsg = MutableLiveData<Int>()
+    fun getSnackbarMsg() = snackbarMsg
+
+    fun consultCouponByCode(code: String) {
+        viewModelScope.launch {
+            result.value = repository.consultCouponByCode(code)
+        }
+    }
+
+    fun saveCoupon(couponEntity: CouponEntity) {
+        viewModelScope.launch {
+            try {
+                repository.saveCoupon(couponEntity)
+                consultCouponByCode(couponEntity.code)
+                snackbarMsg.value = R.string.main_save_sucess
+            } catch (e: Exception) {
+                snackbarMsg.value = getMessageErrorByCode(e.message)
+            }
+        }
+    }
+
 }
