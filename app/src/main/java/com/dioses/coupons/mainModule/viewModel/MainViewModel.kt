6bbe14e89ee6cef4f8e7d071cbcbd2852f7e7d18
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-12-20 12:03:43 -0500

    Impl() - Change coupon by result in MainViewModel.kt.

--- app/src/main/java/com/dioses/coupons/mainModule/viewModel/MainViewModel.kt
+++ app/src/main/java/com/dioses/coupons/mainModule/viewModel/MainViewModel.kt
@@ -12,26 +12,34 @@ import kotlinx.coroutines.launch
 class MainViewModel : ViewModel() {
     private val repository = MainRepository()
 
-    private val result = MutableLiveData<CouponEntity>()
-    fun getResult() = result
+    val coupon = MutableLiveData<CouponEntity>()
+
+    private val hideKeyboard = MutableLiveData<Boolean>()
+    fun isHideKeyboard() = hideKeyboard
 
     private val snackbarMsg = MutableLiveData<Int>()
     fun getSnackbarMsg() = snackbarMsg
 
-    fun consultCouponByCode(code: String) {
-        viewModelScope.launch {
-            result.value = repository.consultCouponByCode(code)
+    fun consultCouponByCode() {
+        coupon.value?.code?.let { code ->
+            viewModelScope.launch {
+                hideKeyboard.value = true
+                coupon.value = repository.consultCouponByCode(code)
+            }
         }
     }
 
-    fun saveCoupon(couponEntity: CouponEntity) {
-        viewModelScope.launch {
-            try {
-                repository.saveCoupon(couponEntity)
-                consultCouponByCode(couponEntity.code)
-                snackbarMsg.value = R.string.main_save_success
-            } catch (e: Exception) {
-                snackbarMsg.value = getMessageErrorByCode(e.message)
+    fun saveCoupon() {
+        coupon.value?.let { couponEntity ->
+            viewModelScope.launch {
+                hideKeyboard.value = true
+                try {
+                    repository.saveCoupon(couponEntity)
+                    consultCouponByCode()
+                    snackbarMsg.value = R.string.main_save_success
+                } catch (e: Exception) {
+                    snackbarMsg.value = getMessageErrorByCode(e.message)
+                }
             }
         }
     }
