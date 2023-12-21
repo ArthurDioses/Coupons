470f766a3e7f3dadd0abd8459a5a30b2e3a9f373
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-12-21 00:34:39 -0500

    Impl() - DataBinding adapters - Create fun bindingIsGone for button create

--- app/src/main/java/com/dioses/coupons/common/adapters/CouponBindingAdapter.kt
+++ app/src/main/java/com/dioses/coupons/common/adapters/CouponBindingAdapter.kt
@@ -1,1 +1,9 @@
 package com.dioses.coupons.common.adapters
+
+import android.view.View
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter("isGone")
+fun bindingIsGone(view: View, isGone: Boolean) {
+    view.visibility = if (isGone) View.GONE else View.VISIBLE
+}
