bdf81cc0e0c438dde826848c4b5c73f2f8a87f38
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-12-21 11:55:30 -0500

    Impl() - DataBinding adapters - Create new BindingAdapter fun bindingTextTitle for change color and text in title coupon

--- app/src/main/java/com/dioses/coupons/common/adapters/CouponBindingAdapter.kt
+++ app/src/main/java/com/dioses/coupons/common/adapters/CouponBindingAdapter.kt
@@ -1,9 +1,23 @@
 package com.dioses.coupons.common.adapters
 
+import android.graphics.Color
 import android.view.View
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.dioses.coupons.R
 
 @BindingAdapter("isGone")
 fun bindingIsGone(view: View, isGone: Boolean) {
     view.visibility = if (isGone) View.GONE else View.VISIBLE
+}
+
+@BindingAdapter("textTitle")
+fun bindingTextTitle(textView: TextView, isActive: Boolean) {
+    if (isActive) {
+        textView.setText(R.string.main_text_title)
+        textView.setTextColor(Color.BLACK)
+    } else {
+        textView.setText(R.string.main_text_title_add_coupon)
+        textView.setTextColor(Color.YELLOW)
+    }
 }
