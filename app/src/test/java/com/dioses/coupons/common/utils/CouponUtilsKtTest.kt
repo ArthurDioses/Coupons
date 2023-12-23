7bb547e9df2746aefbaf42eac2248befa47b5a7a
Arthur Dioses <adiosesr@proveedor.intercorp.com.pe>
2023-12-22 20:56:23 -0500

    Impl() - First unit test with @Test

---
+++ app/src/test/java/com/dioses/coupons/common/utils/CouponUtilsKtTest.kt
@@ -0,0 +1,13 @@
+package com.dioses.coupons.common.utils
+
+import junit.framework.TestCase.assertTrue
+import org.junit.Test
+
+class CouponUtilsKtTest {
+
+    @Test
+    fun validateTextCodeSuccessTest() {
+        assertTrue(true)
+    }
+
+}
